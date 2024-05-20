e761d9e22dab28d540d664e11adc8c769f9ed6e4
Loki <121497865+Jitroy45@users.noreply.github.com>
2024-05-20 18:08:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+<!-- Profile README Template -->
+
+<h1 align="center">Hi 👋, I'm [Your Name]</h1>
+<h3 align="center">A passionate developer proficient in Python, C, C++, and JavaScript</h3>
+
+<p align="center">
+  <img src="https://komarev.com/ghpvc/?username=yourusername&label=Profile%20views&color=0e75b6&style=flat" alt="yourusername" />
+</p>
+
+<p align="center">
+  <a href="https://twitter.com/yourusername" target="blank">
+    <img src="https://img.shields.io/twitter/follow/yourusername?logo=twitter&style=for-the-badge" alt="yourusername" />
+  </a>
+</p>
+
+<p align="center">
+  <a href="https://github.com/ryo-ma/github-profile-trophy">
+    <img src="https://github-profile-trophy.vercel.app/?username=yourusername" alt="yourusername" />
+  </a>
+</p>
+
+<!-- About Me -->
+- 🔭 I’m currently working on [Your Project Name](https://github.com/yourusername/yourprojectname)
+- 🌱 I’m currently learning **[New Technology/Framework]**
+- 👯 I’m looking to collaborate on **[Your Collaboration Interests]**
+- 🤝 I’m looking for help with **[Your Request for Help]**
+- 💬 Ask me about **Python, C, C++, JavaScript**
+- 📫 How to reach me **[your.email@example.com]**
+- 📄 Know about my experiences [Resume](https://yourwebsite.com/resume)
+- ⚡ Fun fact **[Interesting Fact About You]**
+
+<!-- Languages and Tools -->
+<h3 align="left">Languages and Tools:</h3>
+<p align="left">
+  <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> 
+  <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="40" height="40"/> </a> 
+  <a href="https://isocpp.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> 
+  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a>
+  <!-- Add more icons as needed -->
+</p>
+
+<!-- GitHub Stats -->
+<h3 align="left">GitHub Stats:</h3>
+<p align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=yourusername&show_icons=true&locale=en" alt="yourusername" />
+</p>
+<p align="center">
+  <img src="https://github-readme-streak-stats.herokuapp.com/?user=yourusername&" alt="yourusername" />
+</p>
+
+<!-- Top Languages -->
+<h3 align="left">Top Languages:</h3>
+<p align="center">
+  <img src="https://github-readme-stats.vercel.app/api/top-langs?username=yourusername&show_icons=true&locale=en&layout=compact" alt="yourusername" />
+</p>
+
+<!-- Socials -->
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+  <a href="https://linkedin.com/in/yourusername" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg" alt="yourusername" height="30" width="40" /></a>
+  <a href="https://stackoverflow.com/users/yourusername" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/stackoverflow.svg" alt="yourusername" height="30" width="40" /></a>
+  <a href="https://twitter.com/yourusername" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg" alt="yourusername" height="30" width="40" /></a>
+  <!-- Add more social links as needed -->
+</p>
